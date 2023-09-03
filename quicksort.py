464c045618549d6f6977b70314c11f8d80b9df99
Marcin Kaczorowski <marcinkaczorowski666@gmail.com>
2023-09-03 09:51:11 +0200

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -12,18 +12,18 @@ def quicksort(arr):
         #  Let's find our pivot for each array
         piv = arr[0]
 
-        #  Now create array with smaller numbers
+        #  Now create array with smaller numbers than our pivot
         less_than_piv = [i for i in arr[1:] if i <= piv]
 
-        #  And array with bigger numbers
+        #  And array with bigger numbers than pivot
         more_than_piv = [i for i in arr[1:] if i > piv]
 
-        #  If you want to see, how is it working inside, 'uncomment' lower line:
+        #  If you want to see, how is it working inside, 'uncomment' lower lined:
         #  print(f"Our pivot is {piv}. Now lets see array with numbers smaller than pivot: {less_than_piv}.\n"
         #  f"And array with numbers bigger than pivot: {more_than_piv}\n")
 
-        #  In the end we return sorted lists added to each other.
-        #  Remember we are using "stack". For more info go to: https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+        #  In the end we return sorted lists added to each other, so we will have one sorted list.
+        #  Remember we are using something called "stack". For more info go to: https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
         return quicksort(less_than_piv) + [piv] + quicksort(more_than_piv)
 
 
@@ -34,5 +34,5 @@ sorted_array = quicksort(array_to_sort)
 #  Assert is perfect method for checking if our code is correct
 assert sorted_array == [0, 4, 5, 21, 76, 90, 112, 123, 128, 333, 449, 4459, 23348, 999001]
 
-#  And time to print results:
+#  Finally it's time to see results:
 print(f"Our array: {array_to_sort}. \nAnd after quicksort: {sorted_array}")
