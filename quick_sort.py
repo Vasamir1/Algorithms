6b028d5766b25ce07ae4690e0e98ff844dc6fb66
Marcin Kaczorowski <marcinkaczorowski666@gmail.com>
2023-09-08 07:56:42 +0200

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -22,7 +22,7 @@ def quicksort(arr):
         #  print(f"Our pivot is {piv}. Now lets see array with numbers smaller than pivot: {less_than_piv}.\n"
         #  f"And array with numbers bigger than pivot: {more_than_piv}\n")
 
-        #  In the end we return sorted lists added to each other, so we will have one sorted list.
+        #  In the end we return sorted lists added to each other, so we will have one sorted list. Check: "Divide and Conquer", for more Details.
         #  Remember we are using something called "stack". For more info go to: https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
         return quicksort(less_than_piv) + [piv] + quicksort(more_than_piv)
 
